2adfdd888b7d001535424f3aa12ea471016fda5c
Ariful Rana <ariful.rana@ideascale.com>
2024-09-19 16:43:02 +0600

    Refactor configuration file

--- src/main/java/com/arifng/play_es/config/ESConfig.java
+++ src/main/java/com/arifng/play_es/config/ESConfig.java
@@ -1,4 +1,4 @@
-package com.arifng.play_es;
+package com.arifng.play_es.config;
 
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
@@ -7,30 +7,11 @@ import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.elasticsearch.support.HttpHeaders;
-
-import javax.annotation.Nonnull;
-import java.util.function.Supplier;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.arifng.play_es.repository")
-public class Config {
-
-    public @Nonnull ClientConfiguration clientConfiguration() {
-        Supplier<HttpHeaders> headersSupplier = () -> {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Content-Type", "application/json");
-            return headers;
-        };
-        return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
-                //.withHeaders(headersSupplier)
-                .build();
-    }
-
+public class ESConfig {
     @Bean
     public RestClient restClient() {
         return RestClient.builder(new HttpHost("localhost", 9200, "http"))
@@ -39,7 +20,6 @@ public class Config {
                 })
                 .setHttpClientConfigCallback(httpClientBuilder -> {
                     httpClientBuilder.disableAuthCaching();
-
                     httpClientBuilder.addInterceptorLast((HttpResponseInterceptor)
                             (response, context) ->
                                     response.addHeader("X-Elastic-Product", "Elasticsearch"));
